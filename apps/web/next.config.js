99612a4adce4f8e77944bdb4ca595c66bfe7157f
e2e2a <emonawong22@gmail.com>
2025-04-28 17:50:36 +0800

    Add

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@prisma/client': path.resolve(__dirname, './node_modules/@prisma/client'),
+    };
+    return config;
+  },
   // webpack: (config, { isServer }) => {
   //   if (!isServer) {
   //     config.resolve.fallback = {
