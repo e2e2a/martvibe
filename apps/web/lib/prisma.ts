ebf7dd4f0adb1aaeff3d453966da5c5eb539eece
e2e2a <emonawong22@gmail.com>
2025-04-28 17:46:32 +0800

    add

--- apps/web/lib/prisma.ts
+++ apps/web/lib/prisma.ts
@@ -1,20 +1,12 @@
-// import { PrismaClient } from '@/generated/prisma';
-
-// const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-// export const prisma =
-//   globalForPrisma.prisma ||
-//   new PrismaClient({
-//     log: ['query'],
-//   });
-
-// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
-
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '../generated/prisma';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma || new PrismaClient();
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
